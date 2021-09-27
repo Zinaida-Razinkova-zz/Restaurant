1020dc2de49cfeb5b9daa7a1a3020b31c42c33c4
Zinaida-Razinkova <68029159+Zinaida-Razinkova@users.noreply.github.com>
2021-09-27 15:28:33 +0300

    fixed container

--- src/js/galerie.js
+++ src/js/galerie.js
@@ -1,41 +1,41 @@
-// window.openFotoGallery = openFotoGallery;
-// window.closeGallery = closeGallery;
-// window.plusSlides = plusSlides;
-// window.fotoSlide = fotoSlide;
-// window.show_Slides = show_Slides;
+window.openFotoGallery = openFotoGallery;
+window.closeGallery = closeGallery;
+window.plusSlides = plusSlides;
+window.fotoSlide = fotoSlide;
+window.show_Slides = show_Slides;
 
-// function openFotoGallery() {
-//   document.getElementById('galerie-modal').style.display = 'block';
-// }
+function openFotoGallery() {
+  document.getElementById('galerie-modal').style.display = 'block';
+}
 
-// function closeGallery() {
-//   document.getElementById('galerie-modal').style.display = 'none';
-// }
+function closeGallery() {
+  document.getElementById('galerie-modal').style.display = 'none';
+}
 
-// var slideIndex = 1;
-// show_Slides(slideIndex);
+var slideIndex = 1;
+show_Slides(slideIndex);
 
-// function plusSlides(n) {
-//   show_Slides((slideIndex += n));
-// }
+function plusSlides(n) {
+  show_Slides((slideIndex += n));
+}
 
-// function fotoSlide(n) {
-//   show_Slides((slideIndex = n));
-// }
+function fotoSlide(n) {
+  show_Slides((slideIndex = n));
+}
 
-// function show_Slides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName('galerie-slide');
+function show_Slides(n) {
+  var i;
+  var slides = document.getElementsByClassName('galerie-slide');
 
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none';
-//   }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
 
-//   slides[slideIndex - 1].style.display = 'block';
-// }
+  slides[slideIndex - 1].style.display = 'block';
+}
